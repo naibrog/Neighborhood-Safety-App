0499ce54952f67b8c10ba4ba20a0f4a3182a4118
suraiya3241 <suraiyashoyab@gmail.com>
2023-12-06 17:17:25 -0500

    boxes

--- Neighborhood Safety App/frmAccountCreation.Designer.cs
+++ Neighborhood Safety App/frmAccountCreation.Designer.cs
@@ -28,12 +28,347 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtLoginDate = new System.Windows.Forms.TextBox();
+            this.btnCreateAccount = new System.Windows.Forms.Button();
+            this.lblZipCode = new System.Windows.Forms.Label();
+            this.txtZipCode = new System.Windows.Forms.TextBox();
+            this.lblState = new System.Windows.Forms.Label();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.lblStreetAddress = new System.Windows.Forms.Label();
+            this.txtStreetAddress = new System.Windows.Forms.TextBox();
+            this.lblPhoneNumber = new System.Windows.Forms.Label();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblBirthDate = new System.Windows.Forms.Label();
+            this.txtBirthDate = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtLoginDate
+            // 
+            this.txtLoginDate.Location = new System.Drawing.Point(143, 167);
+            this.txtLoginDate.Margin = new System.Windows.Forms.Padding(4);
+            this.txtLoginDate.Name = "txtLoginDate";
+            this.txtLoginDate.Size = new System.Drawing.Size(132, 20);
+            this.txtLoginDate.TabIndex = 56;
+            // 
+            // btnCreateAccount
+            // 
+            this.btnCreateAccount.Location = new System.Drawing.Point(385, 419);
+            this.btnCreateAccount.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCreateAccount.Name = "btnCreateAccount";
+            this.btnCreateAccount.Size = new System.Drawing.Size(241, 111);
+            this.btnCreateAccount.TabIndex = 55;
+            this.btnCreateAccount.Text = "Create Account";
+            this.btnCreateAccount.UseVisualStyleBackColor = true;
+            // 
+            // lblZipCode
+            // 
+            this.lblZipCode.AutoSize = true;
+            this.lblZipCode.Location = new System.Drawing.Point(693, 337);
+            this.lblZipCode.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblZipCode.Name = "lblZipCode";
+            this.lblZipCode.Size = new System.Drawing.Size(59, 15);
+            this.lblZipCode.TabIndex = 54;
+            this.lblZipCode.Text = "Zip Code:";
+            // 
+            // txtZipCode
+            // 
+            this.txtZipCode.Location = new System.Drawing.Point(819, 333);
+            this.txtZipCode.Margin = new System.Windows.Forms.Padding(4);
+            this.txtZipCode.Name = "txtZipCode";
+            this.txtZipCode.Size = new System.Drawing.Size(132, 20);
+            this.txtZipCode.TabIndex = 53;
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(710, 285);
+            this.lblState.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(38, 15);
+            this.lblState.TabIndex = 52;
+            this.lblState.Text = "State:";
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(819, 279);
+            this.txtState.Margin = new System.Windows.Forms.Padding(4);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(132, 20);
+            this.txtState.TabIndex = 51;
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(722, 229);
+            this.lblCity.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(29, 15);
+            this.lblCity.TabIndex = 50;
+            this.lblCity.Text = "City:";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(819, 222);
+            this.txtCity.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(132, 20);
+            this.txtCity.TabIndex = 49;
+            // 
+            // lblStreetAddress
+            // 
+            this.lblStreetAddress.AutoSize = true;
+            this.lblStreetAddress.Location = new System.Drawing.Point(677, 173);
+            this.lblStreetAddress.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStreetAddress.Name = "lblStreetAddress";
+            this.lblStreetAddress.Size = new System.Drawing.Size(89, 15);
+            this.lblStreetAddress.TabIndex = 48;
+            this.lblStreetAddress.Text = "Street Address:";
+            // 
+            // txtStreetAddress
+            // 
+            this.txtStreetAddress.Location = new System.Drawing.Point(819, 167);
+            this.txtStreetAddress.Margin = new System.Windows.Forms.Padding(4);
+            this.txtStreetAddress.Name = "txtStreetAddress";
+            this.txtStreetAddress.Size = new System.Drawing.Size(132, 20);
+            this.txtStreetAddress.TabIndex = 47;
+            // 
+            // lblPhoneNumber
+            // 
+            this.lblPhoneNumber.AutoSize = true;
+            this.lblPhoneNumber.Location = new System.Drawing.Point(677, 109);
+            this.lblPhoneNumber.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPhoneNumber.Name = "lblPhoneNumber";
+            this.lblPhoneNumber.Size = new System.Drawing.Size(94, 15);
+            this.lblPhoneNumber.TabIndex = 46;
+            this.lblPhoneNumber.Text = "Phone Number:";
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(819, 109);
+            this.txtPhoneNumber.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(132, 20);
+            this.txtPhoneNumber.TabIndex = 45;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(715, 61);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(42, 15);
+            this.lblEmail.TabIndex = 44;
+            this.lblEmail.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(819, 53);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(132, 20);
+            this.txtEmail.TabIndex = 43;
+            // 
+            // lblBirthDate
+            // 
+            this.lblBirthDate.AutoSize = true;
+            this.lblBirthDate.Location = new System.Drawing.Point(710, 2);
+            this.lblBirthDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblBirthDate.Name = "lblBirthDate";
+            this.lblBirthDate.Size = new System.Drawing.Size(61, 15);
+            this.lblBirthDate.TabIndex = 42;
+            this.lblBirthDate.Text = "BirthDate:";
+            // 
+            // txtBirthDate
+            // 
+            this.txtBirthDate.Location = new System.Drawing.Point(819, 2);
+            this.txtBirthDate.Margin = new System.Windows.Forms.Padding(4);
+            this.txtBirthDate.Name = "txtBirthDate";
+            this.txtBirthDate.Size = new System.Drawing.Size(132, 20);
+            this.txtBirthDate.TabIndex = 41;
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(23, 304);
+            this.lblLastName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(70, 15);
+            this.lblLastName.TabIndex = 40;
+            this.lblLastName.Text = "Last Name:";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(143, 297);
+            this.txtLastName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(132, 20);
+            this.txtLastName.TabIndex = 39;
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(23, 231);
+            this.lblFirstName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(70, 15);
+            this.lblFirstName.TabIndex = 38;
+            this.lblFirstName.Text = "First Name:";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(143, 225);
+            this.txtFirstName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(132, 20);
+            this.txtFirstName.TabIndex = 37;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Location = new System.Drawing.Point(21, 163);
+            this.lblLogin.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(70, 15);
+            this.lblLogin.TabIndex = 36;
+            this.lblLogin.Text = "Login Date:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(29, 113);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(64, 15);
+            this.lblPassword.TabIndex = 35;
+            this.lblPassword.Text = "Password:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(143, 109);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(132, 20);
+            this.txtPassword.TabIndex = 34;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(23, 53);
+            this.lblUserName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(68, 15);
+            this.lblUserName.TabIndex = 33;
+            this.lblUserName.Text = "Username:";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(143, 53);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(132, 20);
+            this.txtUserName.TabIndex = 32;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(51, 12);
+            this.lblID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(22, 15);
+            this.lblID.TabIndex = 31;
+            this.lblID.Text = "ID:";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(143, 6);
+            this.txtID.Margin = new System.Windows.Forms.Padding(4);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(132, 20);
+            this.txtID.TabIndex = 30;
+            // 
+            // frmAccountCreation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(980, 509);
+            this.Controls.Add(this.txtLoginDate);
+            this.Controls.Add(this.btnCreateAccount);
+            this.Controls.Add(this.lblZipCode);
+            this.Controls.Add(this.txtZipCode);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.lblStreetAddress);
+            this.Controls.Add(this.txtStreetAddress);
+            this.Controls.Add(this.lblPhoneNumber);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblBirthDate);
+            this.Controls.Add(this.txtBirthDate);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.txtID);
+            this.Name = "frmAccountCreation";
             this.Text = "frmAccountCreation";
+            this.Load += new System.EventHandler(this.frmAccountCreation_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtLoginDate;
+        private System.Windows.Forms.Button btnCreateAccount;
+        private System.Windows.Forms.Label lblZipCode;
+        private System.Windows.Forms.TextBox txtZipCode;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Label lblStreetAddress;
+        private System.Windows.Forms.TextBox txtStreetAddress;
+        private System.Windows.Forms.Label lblPhoneNumber;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblBirthDate;
+        private System.Windows.Forms.TextBox txtBirthDate;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtID;
     }
 }
--- Neighborhood Safety App/frmAccountCreation.cs
+++ Neighborhood Safety App/frmAccountCreation.cs
@@ -14,9 +14,69 @@ namespace Neighborhood_Safety_App
     public partial class frmAccountCreation : Form
     {
         LoginDB loginDB = new LoginDB();
+
         public frmAccountCreation()
         {
             InitializeComponent();
+        }
+
+        private void btnCreateAccount_Click(object sender, EventArgs e)
+        {
+            // Validate the input fields
+            if (!ValidateInput())
+            {
+                // Display an error message if validation fails
+                MessageBox.Show("Please fill in all required fields.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; // Exit the method if validation fails
+            }
+
+            // Your account creation logic goes here...
+
+            // Display a success message
+            MessageBox.Show("Account successfully created!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Clear the form or perform any other necessary actions
+            ClearForm();
+        }
+
+        // Method to validate input fields
+        private bool ValidateInput()
+        {
+            // Add your validation logic here
+            // For example, check if required fields are not empty
+            if (string.IsNullOrWhiteSpace(txtUserName.Text) || string.IsNullOrWhiteSpace(txtPassword.Text))
+            {
+                return false;
+            }
+
+            // Add more validation checks as needed...
+
+            return true; // Validation successful
+        }
+
+        // Method to clear the form
+        private void ClearForm()
+        {
+            // Clear all input fields
+            txtID.Text = "";
+            txtUserName.Text = "";
+            txtPassword.Text = "";
+            txtLoginDate.Text = "";
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtBirthDate.Text = "";
+            txtEmail.Text = "";
+            txtPhoneNumber.Text = "";
+            txtStreetAddress.Text = "";
+            txtCity.Text = "";
+            txtState.Text = "";
+            txtZipCode.Text = "";
+            btnCreateAccount.BackColor = System.Drawing.Color.AliceBlue;
+        }
+
+        private void frmAccountCreation_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
